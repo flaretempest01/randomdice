4965e6ef61bbec07329c401d6c1432b8b3404ef7
Kazuya Sato <kazuyasato01@yahoo.com>
2021-02-20 23:04:24 +0800

    initial upload of random dice

---
+++ index.js
@@ -0,0 +1,24 @@
+
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[0].getAttribute("src");
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+
+document.querySelectorAll("img")[1].getAttribute("src");
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = ("Player 1 Win!");
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = ("Player 2 Win!");
+}
+else if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerHTML = ("DRAW");
+}
